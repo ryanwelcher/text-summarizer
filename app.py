a5492745ac77c7c926cb3797833fe4e8a4e5e6c4
Ryan Welcher <me@ryanwelcher.com>
2023-03-21 14:39:20 -0400

    Add input to adjust the prompt sent to gpt

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import openai
 import os
-# from text_summarizer.functions import summarize
 from text_summarizer.functions import summarizeChat
 
 try:
@@ -10,13 +9,15 @@ try:
   if "summary" not in st.session_state:
       st.session_state["summary"] = ""
   
-  st.title("Text Summarizer")
+  st.title("Transcript Summarizer")
+
+  prompt_text = st.text_area(label="Prompt to use:", value="summarize this video transcript text:")
   
   input_text = st.text_area(label="Enter full text:", value="", height=250)
   st.button(
       "Submit",
       on_click=summarizeChat,
-      kwargs={"prompt": input_text},
+      kwargs={"text": input_text, "prompt": prompt_text},
   )
   output_text = st.text_area(label="Summarized text:", value=st.session_state["summary"], height=250)
 except:
--- text_summarizer/functions.py
+++ text_summarizer/functions.py
@@ -1,15 +1,15 @@
 import openai
 import streamlit as st
 
-def summarizeChat(prompt):
-    augmented_prompt = f"summarize this video transcript text: {prompt}"
+def summarizeChat(prompt,text):
+    augmented_text = f"{prompt}: {text}"
     try:
         st.session_state["summary"] = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             temperature= 0.7,
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content":  augmented_prompt}
+                {"role": "user", "content":  augmented_text}
                 ]
             )["choices"][0]["message"]["content"]
     except:
